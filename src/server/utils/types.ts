3c81c873b5d3aed178170b7979827787b0fea18f
Todoo <todoo.zorigtbaatar@gmail.com>
2025-04-15 17:29:04 +0800

    12 sign up backend (#21)
    * signup backend
    
    * Ignore package-lock.json
    
    * signin & get-user-by-id BE
    
    * staff CRUD BE

--- src/server/utils/types.ts
+++ src/server/utils/types.ts
@@ -1,4 +1,10 @@
 import { Schema } from "mongoose";
+import {
+  AppointmentStatusEnum,
+  CategoryEnum,
+  ServiceEnum,
+  StaffRoleEnum,
+} from "../constant";
 
 export interface UserType {
   _id: Schema.Types.ObjectId;
@@ -7,4 +13,41 @@ export interface UserType {
   phoneNumber: string;
   createdAt: Date;
   updatedAt: Date;
+}
+
+export interface StaffType {
+  _id: Schema.Types.ObjectId;
+  email: string;
+  password: string;
+  role: StaffRoleEnum;
+  name: string;
+  experience: number;
+  about: string;
+  rating: number;
+  image: string;
+  category: CategoryEnum;
+  availableTimes: Date[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface AppointmentType {
+  _id: Schema.Types.ObjectId;
+  userId: UserType;
+  staffId: StaffType;
+  date: Date;
+  status: AppointmentStatusEnum;
+  paid: boolean;
+  service: ServiceEnum;
+  price: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface FeedbackType {
+  _id: Schema.Types.ObjectId;
+  userId: UserType;
+  staffId: StaffType;
+  rating: number;
+  createdAt?: Date;
 }

e635a69d55da39b04d45bbf9c13d5d21fb89b5b8
24LP0418 24LP0418 <24LP0418@G304-CC0216.local>
2025-04-16 13:05:25 +0800

    booking begin

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const BookingPage = () => {
+  return (
+    <div className="max-w-[480px] w-full px-4 bg-[#ffffff] ">
+      <h2 className="text-[30px] font-[700] ">Book Your Appointment</h2>
+      <h4 className="text-[14px] font-[500] text-[#6C727F] ">
+        Select a date and time for your service{" "}
+      </h4>
+    </div>
+  );
+};
+
+export default BookingPage;

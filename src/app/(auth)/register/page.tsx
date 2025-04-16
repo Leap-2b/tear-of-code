fb894c090ff45bb855ba24665785cca7adc83454
24LP2291 24LP2291 <24LP2291@G304-CC0221.local>
2025-04-16 12:24:27 +0800

    zasvar

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import { TabsDemo } from "./_compoments/Tabsdemo";
 
 const Page = () => {
   return (
-    <div className="max-w-[480px] w-full h-screen bg-[#f0f0f0] p-4 mx-auto">
+    <div className="max-w-[480px] w-full h-screen bg-[#ffffff] p-4 mx-auto">
       <RegisterHeader />
       <TabsDemo/>
     </div>

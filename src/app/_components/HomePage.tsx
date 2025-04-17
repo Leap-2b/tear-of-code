43692a28e81f2d77a3254ddf91bda19b6f33ac67
Sapar Smagul <smagulsapar52@gmail.com>
2025-04-17 11:59:49 +0800

    Home page Responsive and Mongolian language enabled (#46)
    Co-authored-by: 24LP0418 24LP0418 <24LP0418@G304-CC0216.local>

--- src/app/_components/HomePage.tsx
+++ src/app/_components/HomePage.tsx
@@ -11,7 +11,7 @@ import React from "react";
 
 export const HomePage = () => {
   return (
-    <div className="max-w-[480px] w-full h-screen overflow-scroll flex flex-col gap-[30px] bg-[#ffffff]  ">
+    <div className="w-full h-full  overflow-scroll flex flex-col gap-[30px] bg-[#ffffff]  ">
       <div className="w-full px-4 flex flex-col gap-[30px] ">
         <Carousel className="w-full ">
           <CarouselContent>
@@ -19,7 +19,7 @@ export const HomePage = () => {
               <CarouselItem key={index}>
                 <div className="p-1">
                   <Card className="cursor-pointer p-0 ">
-                    <CardContent className="flex  h-[350px] items-center justify-center p-6">
+                    <CardContent className="flex  h-[350px] sm:h-[400px] items-center justify-center p-6">
                       <span className="text-4xl font-semibold">
                         {index + 1}
                       </span>
@@ -31,39 +31,40 @@ export const HomePage = () => {
           </CarouselContent>
         </Carousel>
         <div className="w-full flex flex-col gap-5 items-center ">
-          <h3 className="text-[36px] font-[700] ">Our services</h3>
-          <div className="flex flex-col gap-[20px] w-full ">
-            <div className="w-full border-black border-[1px] cursor-pointer hover:opacity-80 overflow-hidden rounded-[10px]  ">
+          <h3 className="text-[36px] font-[700] ">Манай үйлчилгээ</h3>
+
+          <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 ">
+            <div className="rounded-lg border bg-card text-card-foreground shadow-sm overflow-hidden transition-all hover:shadow-lg ">
               <Image
                 alt=""
                 src={
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ26y2JHp0GYfv2fqEQNVdWZh3wwwj-7qC96w&s"
+                  "https://menshaircuts.com/wp-content/uploads/2024/06/tp-best-haircuts-for-men.jpg"
                 }
                 height={500}
                 width={500}
-                className="w-full h-[250px] center bg-gray-200 "
+                className="aspect-video w-full overflow-hidden"
               />
-              <div className="bg-background w-full flex flex-col gap-1 p-4 ">
-                <h4 className="text-[16px] font-[700] ">Haircut</h4>
+              <div className="p-4">
+                <h4 className="text-[16px] font-[700] ">Үс засах</h4>
                 <p className="text-[14px] font-[500] text-[#6C727F] ">
-                  Professional haircuts for all styles
+                  Бүх загварт тохирсон мэргэжлийн үс засалт
                 </p>
               </div>
             </div>
-            <div className="w-full border-black border-[1px] cursor-pointer hover:opacity-80 overflow-hidden rounded-[10px]  ">
+            <div className="rounded-lg border bg-card text-card-foreground shadow-sm overflow-hidden transition-all hover:shadow-lg ">
               <Image
                 alt=""
                 src={
-                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ26y2JHp0GYfv2fqEQNVdWZh3wwwj-7qC96w&s"
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9RW5pVVJGFs3n5coTE2LTOgvrTxoBQd52LQ&s"
                 }
                 height={500}
                 width={500}
-                className="w-full h-[250px] bg-gray-200 "
+                className="aspect-video w-full overflow-hidden"
               />
-              <div className="bg-background w-full flex flex-col gap-1 p-4 ">
-                <h4 className="text-[16px] font-[700] ">Haircut</h4>
+              <div className="p-4 ">
+                <h4 className="text-[16px] font-[700] ">Хумсны үйлчилгээ</h4>
                 <p className="text-[14px] font-[500] text-[#6C727F] ">
-                  Professional haircuts for all styles
+                  маникюр, педикюр
                 </p>
               </div>
             </div>
@@ -71,39 +72,41 @@ export const HomePage = () => {
         </div>
       </div>
       <div className="w-full  bg-[#F9FAFB] py-[45px] flex flex-col items-center gap-[45px] ">
-        <h4 className="text-[36px] font-[700] ">How it works</h4>
+        <h4 className="text-[36px] font-[700] text-center ">
+          Апп хэрэглэх алхам
+        </h4>
         <div className="flex flex-col gap-[40px] ">
           <div className="flex flex-col items-center gap-[12px] ">
             <div className="w-16 h-16 rounded-full bg-black text-white flex justify-center items-center ">
               1
             </div>
-            <h4 className="text-[16px] font-[700] ">Choose a service</h4>
-            <p className="text-[14px] font-[500] text-[#6c727f] ">
-              Browse our range of professional services
+            <h4 className="text-[16px] font-[700] ">Үйлчилгээ сонгох</h4>
+            <p className="text-[14px] font-[500] text-center text-[#6c727f] ">
+              Манай мэргэжлийн үйлчилгээтэй танилцана уу
             </p>
           </div>
           <div className="flex flex-col items-center gap-[12px] ">
             <div className="w-16 h-16 rounded-full bg-black text-white flex justify-center items-center ">
-              1
+              2
             </div>
-            <h4 className="text-[16px] font-[700] ">Choose a service</h4>
-            <p className="text-[14px] font-[500] text-[#6c727f] ">
-              Browse our range of professional services
+            <h4 className="text-[16px] font-[700] ">Үсчингээ сонго</h4>
+            <p className="text-[14px] font-[500] text-center text-[#6c727f] ">
+              Дуртай стилист болон цагийн хуваарийг сонго
             </p>
           </div>
           <div className="flex flex-col items-center gap-[12px] ">
             <div className="w-16 h-16 rounded-full bg-black text-white flex justify-center items-center ">
-              1
+              3
             </div>
-            <h4 className="text-[16px] font-[700] ">Choose a service</h4>
-            <p className="text-[14px] font-[500] text-[#6c727f] ">
-              Browse our range of professional services
+            <h4 className="text-[16px] font-[700] ">Баталгаажуулж төлнө</h4>
+            <p className="text-[14px] font-[500] text-center text-[#6c727f] ">
+              Төлбөрийг хялбархан хийж цагаа баталгаажуулаарай
             </p>
           </div>
         </div>
         <div>
           <Link href={"/service"}>
-            <Button className="cursor-pointer ">Book Now</Button>
+            <Button className="cursor-pointer ">Oдоо захиалаарай</Button>
           </Link>
         </div>
       </div>

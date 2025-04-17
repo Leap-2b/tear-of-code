27dc5ff798fa047e5fc66ec592bbf16f4a0e948d
24LP2291 24LP2291 <24LP2291@G304-CC0221.local>
2025-04-17 13:03:57 +0800

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Header } from "./_components/Header";
-import { StaffProvider } from "./_context/StaffContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,8 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
-        </div>
+        <div>{children}</div>
       </body>
     </html>
   );

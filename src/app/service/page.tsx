9c5eda05cc76c5583452439bdb96d11b7f04353c
24LP0542 24LP0542 <24LP0542@admins-iMac.local>
2025-04-22 09:42:45 +0800

    service

--- src/app/service/page.tsx
+++ src/app/service/page.tsx
@@ -6,29 +6,35 @@ import HairCut from "./components/HairCut";
 import All from "./components/All";
 import { useEffect, useState } from "react";
 import { ServiceType } from "@/server/utils";
+import axios from "axios";
 
 export default function Service() {
   const [service, setService] = useState<ServiceType[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
     const getAllService = async () => {
       try {
-        const res = await fetch("/api/service");
-        const json = await res.json();
-        setService(json.data);
-      } catch (err) {
+        const { data } = await axios.get("/api/service");
+
+        console.log("SERVICES", data);
+        setService(data.data);
+      } catch (err: any) {
         console.error("Алдаа гарлаа:", err);
+        setError("Үйлчилгээнүүдийг аяааллаж чадсангүй.");
+      } finally {
+        setLoading(false);
       }
     };
     getAllService();
   }, []);
-  console.log(service);
   return (
     <div className="w-full  h-full px-[16px] bg-white pt-4">
       <Tabs defaultValue="all">
         <TabsList className="px-4 py-1 bg-blue-50 w-full">
           <TabsTrigger value="all">Бүх үйлчилгээ</TabsTrigger>
           <TabsTrigger value="haircuts">Үсний засалтууд</TabsTrigger>
-          <TabsTrigger value="nails">Хумсны будалтууд /</TabsTrigger>
+          <TabsTrigger value="nails">Хумсны будалт </TabsTrigger>
         </TabsList>
         <TabsContent value="all">
           <All service={service} />

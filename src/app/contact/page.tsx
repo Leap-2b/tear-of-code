cd0b443aa07ab05238d19cdd6ad2d7a2323a981b
OchirooHub <122595698+Ochir00@users.noreply.github.com>
2025-04-28 11:28:48 +0800

    contact page copy (#124)
    Co-authored-by: 24LP2291 24LP2291 <24LP2291@G304-CC0221.local>

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,16 +1,25 @@
-import AskedQuest from "../service/_components/AskedQuest";
-import { Contact } from "./_compoments/Contact";
-import { GetImTouchHeader } from "./_compoments/GetInTouchHeader";
+"use client";
+import type React from "react";
+import { ContactInformation } from "./_components/ContactInformation";
+import { ContactForm } from "./_components/ContactForm";
+import { FAQSection } from "./_components/FAQSection";
+import { HeroSection } from "./_components/ HeroSection";
 
-export default function MyComponent() {
+export default function ContactPage() {
   return (
-    <div className="max-w-[768px] w-full">
-      {/* Deed tolgoi hesegiin black box */}
-      <GetImTouchHeader />
-      <div className="w-full">
-        <Contact />
-      </div>
-      <AskedQuest />
+    <div className="flex min-h-screen flex-col">
+      <HeroSection />
+      <section className="py-16 md:py-24">
+        <div className="max-w-[1400px] m-auto px-4 md:px-6">
+          <div className="grid gap-10 md:grid-cols-2">
+            <ContactInformation />
+            <div>
+              <ContactForm />
+            </div>
+          </div>
+        </div>
+      </section>
+      <FAQSection />
     </div>
   );
 }
